c2929d5dcb7e4e5f5a417fc77fe0d68ab54bbf90
Mélusine PULIERO <96616995+meowlusine@users.noreply.github.com>
2026-04-08 11:25:36 +0200

    Update Joueur.h

--- Joueur.h
+++ Joueur.h
@@ -1,12 +1,18 @@
+#include<iostream>
+using namespace std ;
+
 class Joueur {
 private:
-    std::string nom;
-    int hp_actuel, hp_max;
-    int nb_victoires, nb_tues, nb_epargnes;
-    std::vector<Item> inventaire;
-    std::vector<EntreeBestiaire> bestiaire;
+    string nom;
+    int hp_actuel;
+    int hp_max;
+    int nb_victoires;
+    int nb_tues;
+    int nb_epargnes;
+    Item*[] inventaire;
+    EntreeBestiaire*[] bestiaire;
 public:
     void utiliserItem(int index);
     void afficherStats() const;
-    std::string getFin() const; // "GENOCIDAIRE" / "PACIFISTE" / "NEUTRE"
+    string getFin() const; // "GENOCIDAIRE" / "PACIFISTE" / "NEUTRE"
 };
