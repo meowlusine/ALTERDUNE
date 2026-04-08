05343edbcd1eec6e74343e99cae9a493842eaac9
Mélusine PULIERO <96616995+meowlusine@users.noreply.github.com>
2026-04-08 12:36:07 +0200

    Add files via upload

--- Joueur.h
+++ Joueur.h
@@ -2,6 +2,7 @@
 using namespace std ;
 
 class Joueur {
+
 private:
     string nom;
     int hp_actuel;
@@ -11,7 +12,46 @@ private:
     int nb_epargnes;
     Item*[] inventaire;
     EntreeBestiaire*[] bestiaire;
+
 public:
+
+    //getters et setters
+
+    string getNom(){ return nom; }
+    int getHpActuel(){ return hp_actuel; }
+    int getHpMax(){return hp_max; }
+    int getNbVictoires(){ return nb_victoires; }
+    int getNbTues(){ return nb_tues; }
+    int getNbEpargnes(){ return nb_epargnes; }
+    Item*[] getInventaire(){ return inventaire; }
+    EntreeBestiaire*[] getBestiaire(){ return bestiaire; }
+
+
+    void setNom(string n)   { nom = n; }
+    void setHpMax(int v) { if (v > 0) hp_max = v; }
+    void setHpActuel(int v) {
+        if(v < 0){
+            hp_actuel = 0;
+        }
+        else if (v > hp_max){
+            hp_actuel = hp_max;
+        }
+        else{
+            hp_actuel = v;
+        }
+    }
+
+    // modif des attributs
+    void incrementerVictoires()  { nb_victoires++; }
+    void incrementerTues()       { nb_tues++; }
+    void incrementerEpargnes()   { nb_epargnes++; }
+
+    void ajouterItem(Item& item){ inventaire.push_back(item); }
+    void ajouterEntreeBestiaire(EntreeBestiaire& e) { bestiaire.push_back(e); }
+
+    bool estVivant(){ return hp_actuel > 0; }
+    bool aGagne(){ return nb_victoires >= 10; }
+
     void utiliserItem(int index);
     void afficherStats() const;
     string getFin() const; // "GENOCIDAIRE" / "PACIFISTE" / "NEUTRE"
