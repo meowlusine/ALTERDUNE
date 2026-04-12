40f79bf92598ccbc3e8d24fdb2869b9bd5e31d59
Mael824 <pagetmael2005@gmail.com>
2026-04-12 16:47:05 +0200

    Update Combat.h

--- Combat.h
+++ Combat.h
@@ -1,11 +1,42 @@
+#ifndef COMBAT_H
+#define COMBAT_H
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include "Joueur.h"
+#include "Monstre.h"
+
+using namespace std;
+
+// Structure pour stocker les données lues depuis le CSV
+struct DonneeMonstre {
+    string categorie;
+    string nom;
+    int hp;
+    int atk;
+    int def;
+    int mercyGoal;
+    vector<string> actionsACT;
+};
+
 class Combat {
 private:
     Joueur& joueur;
     Monstre* monstre;
+    bool combatGagne;
+
+    void genererMonstreAleatoire(const vector<DonneeMonstre>& bdd);
+
 public:
-    void lancer();       // boucle principale
+    Combat(Joueur& j, const vector<DonneeMonstre>& bdd);
+    ~Combat();
+
+    void lancer();
     void fight();
     void act(int choix);
     void mercy();
     void item(int index);
 };
+
+#endif
